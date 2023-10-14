78eda307bb2d021b208b2b70acda6f889d0bc874
Luke Weisenbach <lukeweisenbach@live.com>
2023-10-14 15:35:24 +0100

    update reading array

--- ncc/ncc_read_write_files.cuh
+++ ncc/ncc_read_write_files.cuh
@@ -1,70 +1,29 @@
 ﻿#pragma once
 
 #include "complex.cuh"
+#include "util.hpp"
 
-#include <cstdint>
+#include <cstdint> //for std::uintmax_t
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <new>
 #include <string>
-#include <system_error>
+#include <system_error> //for std::error_code
 
-
-/******************************************************************************
-read nrows and ncols parameters from file
-
-\param nrows -- number of rows in array
-\param ncols -- number of columns in array
-\param fname -- location of the file to read from
-
-\return bool -- true if file is successfully read, false if not
-******************************************************************************/
-template<typename T>
-bool read_params(int& nrows, int& ncols, const std::string& fname)
-{
-	std::filesystem::path fpath = fname;
-
-	if (fpath.extension() != ".bin")
-	{
-		std::cerr << "Error. File " << fname << " is not a .bin file.\n";
-		return false;
-	}
-
-	std::ifstream infile;
-
-	infile.open(fname, std::ios_base::binary);
-
-	if (!infile.is_open())
-	{
-		std::cerr << "Error. Failed to open file " << fname << "\n";
-		return false;
-	}
-
-	infile.read((char*)(&nrows), sizeof(int));
-	infile.read((char*)(&ncols), sizeof(int));
-	infile.close();
-	if (nrows < 1 || ncols < 2)
-	{
-		std::cerr << "Error. File " << fname << " does not contain valid values for num_rows and num_cols.\n";
-		return false;
-	}
-
-	return true;
-}
 
 /******************************************************************************
 read array of complex values from disk into array
 
 \param vals -- pointer to array of values
-\param nrows -- number of rows in array
-\param ncols -- number of columns in array
+\param nrows -- pointer to number of rows in array
+\param ncols -- pointer to number of columns in array
 \param fname -- location of the .bin file to read from
 
 \return bool -- true if file is successfully read, false if not
 ******************************************************************************/
 template <typename T>
-bool read_complex_array(Complex<T>* vals, int nrows, int ncols, const std::string& fname)
+bool read_complex_array(Complex<T>*& vals, int nrows, int ncols, const std::string& fname)
 {
 	std::filesystem::path fpath = fname;
 
@@ -93,15 +52,32 @@ bool read_complex_array(Complex<T>* vals, int nrows, int ncols, const std::strin
 		return false;
 	}
 
-	int temp;
-	infile.read((char*)(&temp), sizeof(int));
-	infile.read((char*)(&temp), sizeof(int));
+	infile.read((char*)(&nrows), sizeof(int));
+	infile.read((char*)(&ncols), sizeof(int));
+	if (nrows < 1 || ncols < 1)
+	{
+		std::cerr << "Error. File " << fname << " does not contain valid values for num_rows and num_cols.\n";
+		return false;
+	}
 
-	if ((fsize - 2 * sizeof(int)) == nrows * ncols * sizeof(Complex<T>))
+	/******************************************************************************
+	allocate memory for array if needed
+	******************************************************************************/
+	if (vals == nullptr)
+	{
+		cudaMallocManaged(&vals, nrows * ncols * sizeof(Complex<T>));
+		if (cuda_error("cudaMallocManaged(*vals)", false, __FILE__, __LINE__)) return false;
+	}
+
+
+	/******************************************************************************
+	objects in the file are nrows + ncols + complex values
+	******************************************************************************/
+	if (fsize == 2 * sizeof(int) + nrows * ncols * sizeof(Complex<T>))
 	{
 		infile.read((char*)vals, nrows * ncols * sizeof(Complex<T>));
 	}
-	else if ((fsize - 2 * sizeof(int)) == nrows * ncols * sizeof(Complex<float>))
+	else if (fsize == 2 * sizeof(int) + nrows * ncols * sizeof(Complex<float>))
 	{
 		Complex<float>* temp_vals = new (std::nothrow) Complex<float>[nrows * ncols];
 		if (!temp_vals)
@@ -112,12 +88,12 @@ bool read_complex_array(Complex<T>* vals, int nrows, int ncols, const std::strin
 		infile.read((char*)temp_vals, nrows * ncols * sizeof(Complex<float>));
 		for (int i = 0; i < nrows * ncols; i++)
 		{
-			vals[i] = Complex<T>(temp_vals[i].re, temp_vals[i].im);
+			vals[i] = Complex<T>(temp_vals[i]);
 		}
 		delete[] temp_vals;
 		temp_vals = nullptr;
 	}
-	else if ((fsize - 2 * sizeof(int)) == nrows * ncols * sizeof(Complex<double>))
+	else if (fsize == 2 * sizeof(int) + nrows * ncols * sizeof(Complex<double>))
 	{
 		Complex<double>* temp_vals = new (std::nothrow) Complex<double>[nrows * ncols];
 		if (!temp_vals)
@@ -128,7 +104,7 @@ bool read_complex_array(Complex<T>* vals, int nrows, int ncols, const std::strin
 		infile.read((char*)temp_vals, nrows * ncols * sizeof(Complex<double>));
 		for (int i = 0; i < nrows * ncols; i++)
 		{
-			vals[i] = Complex<T>(temp_vals[i].re, temp_vals[i].im);
+			vals[i] = Complex<T>(temp_vals[i]);
 		}
 		delete[] temp_vals;
 		temp_vals = nullptr;
@@ -136,7 +112,6 @@ bool read_complex_array(Complex<T>* vals, int nrows, int ncols, const std::strin
 	else
 	{
 		std::cerr << "Error. Binary file does not contain valid single or double precision numbers.\n";
-		infile.close();
 		return false;
 	}
 
